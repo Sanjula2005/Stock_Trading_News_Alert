d7e4335b187127e278f4677730d27e59ad0bd995
Sanjula.S <sanjulasudhindra12@gmail.com>
2024-12-11 22:42:18 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY="ROE7564GYS6IMH8J"
+NEWS_API_KEY="8ab6dd5e8f54435fbecb8653595fad5f"
+TWILIO_SID="ACdf5bb394135643318550bf0353489661"
+TWILIO_AUTH_TOKEN="8e7b7dd3c663ddd809fe685683c096b8"
+
+
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
+
+
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+
+    three_articles = articles[:3]
+    print(three_articles)
+
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+17755725983",
+            to="+917019612411"
+        )
